722a9be7839ec7776bbf67e52827aa65520573d9
324697523 <324697523@SL-5CG6164KWM.tdsb.on.ca>
2018-03-21 11:21:54 -0400

    a

--- unit 1/src/tan/ch3/Spending.java
+++ unit 1/src/tan/ch3/Spending.java
@@ -19,10 +19,17 @@ public class Spending {
 		double entertainmentPercent = 0;
 		double rentPercent = 0;
 		
+		System.out.println("Enter the amount spent last month on the following items: ");
+		System.out.println(" ");
+		System.out.println("Food:");
 		food = input.nextDouble();
+		System.out.println("Clothing:");
 		clothing = input.nextDouble();
+		System.out.println("Entertainment:");
 		entertainment = input.nextDouble();
+		System.out.println("Rent:");
 		rent = input.nextDouble();
+		System.out.println(" ");
 		
 		total = food + clothing + entertainment + rent;
 		foodPercent = (food / total);
@@ -30,8 +37,9 @@ public class Spending {
 		entertainmentPercent = (entertainment / total);
 		rentPercent = (rent / total);
 		
-		System.out.println( df.format (foodPercent));
-		System.out.println( df.format (clothingPercent));
+		System.out.println("Category	Budget");
+		System.out.println("Food		" + df.format (foodPercent));
+		System.out.println("Clothing	" + df.format (clothingPercent));
 		System.out.println( df.format (entertainmentPercent));
 		System.out.println( df.format (rentPercent));
 		// TODO Auto-generated method stub
