b29ca3006faec89b3dabcd50bb3d4fd7b606b277
324697523 <324697523@SL-5CG6164KWM.tdsb.on.ca>
2018-04-03 11:13:35 -0400

    a

---
+++ unit 1/src/tan/ch4/Grade.java
@@ -0,0 +1,19 @@
+package tan.ch4;
+
+import java.util.Scanner;
+
+public class Grade {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		double grade = 0;
+		char letter = 0;
+		
+		grade = input.nextDouble();
+		
+		if(grade >= 90) {
+			letter = A;}
+
+	}
+
+
