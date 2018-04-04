578d4a405252094310f894b0e5897c785260f633
324697523 <324697523@SL-5CG6164KWM.tdsb.on.ca>
2018-04-04 10:51:57 -0400

    a

--- unit 1/src/tan/ch4/Grade.java
+++ unit 1/src/tan/ch4/Grade.java
@@ -2,18 +2,33 @@ package tan.ch4;
 
 import java.util.Scanner;
 
+/**
+ * Grade.java
+ * 
+ * Apr,4,2018
+ * @author Ian Tan
+ */
+
 public class Grade {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		double grade = 0;
-		char letter = 0;
+		char letter = 'F';
 		
+		System.out.println("Enter the percentage");
 		grade = input.nextDouble();
 		
 		if(grade >= 90) {
-			letter = A;}
+			letter = 'A';}
+		else if(grade >= 80) {
+			letter = 'B';}
+		else if(grade >= 70) {
+			letter = 'C';}
+		else if(grade >= 60) {
+			letter = 'D';}
+		System.out.println("The corresponding letter grade is: " + letter);
 
 	}
-
+}
 
